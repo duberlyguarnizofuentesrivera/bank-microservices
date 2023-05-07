9fca49a0e8e2f172f354b721c925b70ca45bc1bc
Duberly Guarnizo <duberlygfr@gmail.com>
2023-05-06 19:10:33 -0500

    microservices initial commit

---
+++ account-info-service/src/main/java/com/duberlyguarnizo/accountinfoservice/AccountInfoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.duberlyguarnizo.accountinfoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountInfoServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountInfoServiceApplication.class, args);
+    }
+
+}
---
+++ account-info-service/src/test/java/com/duberlyguarnizo/accountinfoservice/AccountInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duberlyguarnizo.accountinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountInfoServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
